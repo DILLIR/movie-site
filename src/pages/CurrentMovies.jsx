23151956ed435f716e3b94efa93c806b15a69c43
DILLIR <uyra908@gmail.com>
2022-08-21 17:41:07 +0300

    changed file structure

--- src/pages/CurrentMovies.jsx
+++ src/pages/CurrentMovies.jsx
@@ -1,58 +1,54 @@
-import React,{useState, useEffect, useRef} from 'react';
-import FilmsBlock from "../components/FilmsBlock";
-import { useFeatching } from '../Hooks/useFeatching';
+import React, {useState, useEffect} from 'react'
+import Baner from '../components/UI/Banner/Baner';
+import banerImg from "../img/geoffrey-moffett-TFRezw7pQwI-unsplash.jpg";
+import FilmsBlock from '../components/FilmsBlock';
 import FilmsService from '../API/FilmsService';
-import {useParams} from "react-router-dom";
 import Loader from '../components/UI/Loader/Loader';
+import { useFeatching } from '../Hooks/useFeatching';
+import { useRef } from 'react';
 import { useObserver } from '../Hooks/useObserver';
 
-function FilmsQuery() {
-  const params = useParams();
 
-  const [films, setFilms] = useState([]);
+function CurrentMovies() {
+
+  const [films, setFilms] = useState({});
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
 
-  const [searchFilms, isLoading, errorFilms, setError, setIsLoading] = useFeatching(async (page, clearPage = false) => {
-    let response = await FilmsService.searchFilms(params.query, page);
+  const [featchFilms, isLoading, errorFilms, setError, setIsLoading] = useFeatching(async (page) => {
+    let response = await FilmsService.getFilms(page);
     if(!response.success && response.success !== undefined){
       setError(response.status_message)
     }else if(page === 1){
-      setPage(1);
       setFilms(response.results);
       setTotalPages(response.total_pages);
     }else{
-      setFilms([...films, ...response.results]);
-    };
+      setFilms([...films,...response.results]);
+    }
   })
-  
-  console.log(page)
 
   useEffect(() => {
-    searchFilms(1,true);
-    // eslint-disable-next-line
-  }, [params.query]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    setTimeout(() => { searchFilms(page)}, 700);
+    featchFilms(page);
     // eslint-disable-next-line
   }, [page]);
 
   const lastElement = useRef();
 
   useObserver(lastElement, page < totalPages, isLoading, ()=>{
-      setPage(p=>p+1);
+      setIsLoading(true);
+      setTimeout(() =>{setPage(p=>p+1);}, 1000);
   })
 
   return (
     <main>
+      <Baner src={banerImg} text="Watch our current premieres"></Baner>
       {errorFilms && <h1 style={{color: "red", textAlign: "center"}}>Error: {errorFilms}</h1>}
-      <FilmsBlock films={films}  name={"Search results - " + params.query} />
+      <FilmsBlock films={films}  name="Now Playing" />
       <div className='odserver' ref={lastElement} style={{height: 30}}></div>
       {isLoading && !errorFilms && <Loader/>}
     </main>
+   
   )
 }
 
-export default FilmsQuery;
+export default CurrentMovies;
--- src/pages/FilmsQuery.jsx
+++ src/pages/FilmsQuery.jsx
@@ -1,54 +1,57 @@
-import React, {useState, useEffect} from 'react'
-import Baner from '../components/UI/Banner/Baner';
-import banerImg from "../img/geoffrey-moffett-TFRezw7pQwI-unsplash.jpg";
-import FilmsBlock from '../components/FilmsBlock';
-import FilmsService from '../API/FilmsService';
-import Loader from '../components/UI/Loader/Loader';
+import React,{useState, useEffect, useRef} from 'react';
+import FilmsBlock from "../components/FilmsBlock";
 import { useFeatching } from '../Hooks/useFeatching';
-import { useRef } from 'react';
+import FilmsService from '../API/FilmsService';
+import {useParams} from "react-router-dom";
+import Loader from '../components/UI/Loader/Loader';
 import { useObserver } from '../Hooks/useObserver';
 
+function FilmsQuery() {
+  const params = useParams();
 
-function CurrentMovies() {
-
-  const [films, setFilms] = useState({});
+  const [films, setFilms] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
 
-  const [featchFilms, isLoading, errorFilms, setError, setIsLoading] = useFeatching(async (page) => {
-    let response = await FilmsService.getFilms(page);
+  const [searchFilms, isLoading, errorFilms, setError, setIsLoading] = useFeatching(async (page, clearPage = false) => {
+    let response = await FilmsService.searchFilms(params.query, page);
     if(!response.success && response.success !== undefined){
       setError(response.status_message)
     }else if(page === 1){
+      setPage(1);
       setFilms(response.results);
       setTotalPages(response.total_pages);
     }else{
-      setFilms([...films,...response.results]);
-    }
+      setFilms([...films, ...response.results]);
+    };
   })
 
   useEffect(() => {
-    featchFilms(page);
+    searchFilms(1,true);
+    // eslint-disable-next-line
+  }, [params.query]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => { searchFilms(page)}, 1000);
     // eslint-disable-next-line
   }, [page]);
 
   const lastElement = useRef();
 
+
   useObserver(lastElement, page < totalPages, isLoading, ()=>{
-      setIsLoading(true);
-      setTimeout(() =>{setPage(p=>p+1);}, 700);
+      setPage(p=>p+1);
   })
 
   return (
     <main>
-      <Baner src={banerImg} text="Watch our current premieres"></Baner>
       {errorFilms && <h1 style={{color: "red", textAlign: "center"}}>Error: {errorFilms}</h1>}
-      <FilmsBlock films={films}  name="Now Playing" />
+      <FilmsBlock films={films}  name={"Search results - " + params.query} />
       <div className='odserver' ref={lastElement} style={{height: 30}}></div>
       {isLoading && !errorFilms && <Loader/>}
     </main>
-   
   )
 }
 
-export default CurrentMovies;
+export default FilmsQuery;
